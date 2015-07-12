889a18d39e8cd227b4fa27e1e4456368ac9a3f02
Byeonghyeon-Lee <inr1319@gmail.com>
2015-07-13 00:08:29 +0900

    commit simple_chat source code for the first time

---
+++ lee/simple_chat/Simple_Chat/Simple_Chat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Simple_Chat
+{
+    public class chatServer
+    {
+        public class AsyncObject
+        {
+            public Byte[] buffer;
+            public Socket WorkingSocket;
+            public AsyncObject(Int32 bufferSize)
+            {
+                this.buffer = new Byte[bufferSize];
+            }
+        }
+    }
+}
